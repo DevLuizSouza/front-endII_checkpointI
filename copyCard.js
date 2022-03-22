dc0e8d23931bb26793f5d11fb0c6ade237d7ab0a
DevLuizSouza <inboxluizsouza@gmail.com>
2022-03-22 20:56:51 -0300

    Terceiro Commit

--- copyCard.js
+++ copyCard.js
@@ -6,13 +6,13 @@ buttonCopy = document.querySelector("#copy")
 buttonCopy.addEventListener("click", function (e) {
     e.preventDefault();
     const copyField1 = document.querySelector("#titleField").value;
-    cole.log(copyField1)
+    console.log(copyField1)
     const copyField2 = document.querySelector("#descriptionField").value;
     console.log(copyField2)
     const copyField3 = document.querySelector("#linkField").value;
     console.log(copyField3)
 
-    if (copyField1.value == "" || copyField2.value == "" || copyField3.value == "") {
+    if (copyField1 == "" || copyField2 == "" || copyField3 == "") {
         alert("Preencha todos os campos!");
     } else {
         const cardsDiv = document.createElement("div");
@@ -25,7 +25,7 @@ buttonCopy.addEventListener("click", function (e) {
         cardCategory.setAttribute("class", "card-category");
 
         // add style to the card-category div
-        cardCategory.setAttribute("style", "background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(https://www.autoinforme.com.br/wp-content/uploads/2020/03/GM-tracker_2021.jpg)");
+        cardCategory.setAttribute("style", "background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url()");
         // add the text of the card
         cardCategory.appendChild(document.createTextNode(copyField1));
         cardCategory.appendChild(document.createTextNode(copyField2));
